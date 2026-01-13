b6d843bba699268cf90701613aaf87ebbaf84204
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2026-01-13 23:00:09 +0900

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -411,6 +411,7 @@ class GPTConfig:
     pos_start : int = 129
     pos_size : int = 32
     bar_id : Optional[int] = 128
+    use_remi_pe : bool = False
 
 class GPT(nn.Module):
 
@@ -565,15 +566,15 @@ class Hyperparameters:
     input_bin : str = 'train.bin' # input .bin to train on
     input_val_bin : str = 'val.bin' # input .bin to eval validation loss on
     # optimization hyperparams
-    batch_size : int = 16 # batch size, in sequences, across all devices
-    sequence_length : int = 2048 # sequence length, in tokens
-    num_iterations : int = 600 # number of iterations to run
+    batch_size : int = 8 # batch size, in sequences, across all devices
+    sequence_length : int = 2048 * 32   # sequence length, in tokens
+    num_iterations : int = 500 # number of iterations to run
     warmup_iters : int = 0
     cooldown_iters : int = 100 # number of iterations of linear warmup/cooldown for triangular or trapezoidal schedule
     weight_decay : float = 0
     # evaluation and logging hyperparams
-    val_loss_every : int = 10 # every how many steps to evaluate val loss? 0 for only at the end
-    val_tokens : int = 4096 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
+    val_loss_every : int = 100 # every how many steps to evaluate val loss? 0 for only at the end
+    val_tokens : int = 2048 * 32 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
     save_every : int = 0 # every how many steps to save the checkpoint? 0 for only at the end
 args = Hyperparameters()
 
